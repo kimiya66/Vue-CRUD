865a912529127c932c5f54b18493ac6669938c0d
kimiya <kimia.beheshti@tum.de>
2019-11-09 01:31:32 +0100

    new changes

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { ClientTable } from 'vue-tables-2';
+
+Vue.use(ClientTable);
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +23,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
